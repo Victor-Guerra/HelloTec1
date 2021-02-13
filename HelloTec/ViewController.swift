525ea95ab01d3e97a38826dc0b35b46733b4d610
ei <simon>
2021-02-12 20:28:05 -0600

    Initial Commit

---
+++ HelloTec/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HelloTec
+//
+//  Created by user190856 on 2/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showMessage(_ sender: UIButton) {
+        var emojiDict = ["👽":"Alien", "🈚️":"Nai", "😡":"im_angy"]
+        let selectedButton = sender
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            let meaning = emojiDict[wordToLookup]
+            let alertController = UIAlertController(title: "Meaning",
+                message: "Hello " + meaning!,
+                preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK",
+                style: UIAlertAction.Style.default,
+                handler: nil))
+            present(alertController, animated: true, completion: nil)
+            
+        }
+    }
+ 
+
+}
+
+
